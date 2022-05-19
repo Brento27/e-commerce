2089c6ff37926ce1c53d6bda1bb3fd8902f5f885
Brento27 <lombaardbrent@gmail.com>
2022-05-19 09:19:35 +0200

    Fixed route that wasn't going to product details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,7 +18,6 @@ const App = () => {
     </main>
     <Footer/>
   </Router>
-    
   )
 }
 
--- frontend/src/sceens/ProductScreen.js
+++ frontend/src/sceens/ProductScreen.js
@@ -1,10 +1,17 @@
 import React from 'react'
+// import { Link } from 'react-router-dom'
+// import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+// import Rating from '../components/Rating'
+// import products from '../products'
 
 const ProductScreen = () => {
+// const product = products.find((p) => p._id === match.params.id)
+
+
   return (
-    <div>
-      
-    </div>
+    <>
+      product
+    </>
   )
 }
 
