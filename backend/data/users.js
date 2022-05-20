823c055510d27284d4a133ec61e4ab34cc742000
Brento27 <lombaardbrent@gmail.com>
2022-05-20 17:57:01 +0200

    Created users.js in backend folder containing our startup users

---
+++ backend/data/users.js
@@ -0,0 +1,27 @@
+import bcrypt from 'bcryptjs'
+
+const user = [
+    {
+        name: 'Admin User',
+        email: 'admin@exapmle.com',
+        password: bcrypt.hashSync('123456', 10),
+        isAdmin: true
+    },
+    {
+        name: 'John Doe',
+        email: 'john@exapmle.com',
+        password: bcrypt.hashSync('123456', 10)
+    },
+    {
+        name: 'Jane Doe',
+        email: 'jane@exapmle.com',
+        password: bcrypt.hashSync('123456', 10)
+    },
+    {
+        name: 'Brent Lombaard',
+        email: 'brent@exapmle.com',
+        password: bcrypt.hashSync('020223', 10)
+    }
+]
+
+export default users
