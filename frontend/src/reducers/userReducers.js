83db85ced52b599516f584a4194d6a01c211e88e
Brento27 <lombaardbrent@gmail.com>
2022-05-25 21:23:56 +0200

    Update User profile implemented

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -9,6 +9,9 @@ import {
   USER_REGISTER_FAIL,
   USER_REGISTER_REQUEST,
   USER_REGISTER_SUCCES,
+  USER_UPDATE_PROFILE_FAIL,
+  USER_UPDATE_PROFILE_REQUEST,
+  USER_UPDATE_PROFILE_SUCCES,
 } from "../constants/userConstants";
 
 export const userLoginReducer = (state = {}, action) => {
@@ -46,6 +49,19 @@ export const userDetailsReducer = (state = { user: {} }, action) => {
     case USER_DETAILS_SUCCES:
       return { loading: false, user: action.payload };
     case USER_DETAILS_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const userUpdateProfilesReducer = (state = { }, action) => {
+  switch (action.type) {
+    case USER_UPDATE_PROFILE_REQUEST:
+      return { loading: true };
+    case USER_UPDATE_PROFILE_SUCCES:
+      return { loading: false, succes: true, userInfo: action.payload };
+    case USER_UPDATE_PROFILE_FAIL:
       return { loading: false, error: action.payload };
     default:
       return state;
