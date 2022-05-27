7b0859153b27e5fdc3fa193876158148a7304367
Brento27 <lombaardbrent@gmail.com>
2022-05-27 14:27:17 +0200

    Added get order by id endpoint in backend

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -15,26 +15,43 @@ const addOrderItems = asyncHandler(async (req, res) => {
     totalPrice,
   } = req.body;
 
-  if(orderItems && orderItems.length === 0) {
-      res.status(400)
-      throw new Error('No order items')
-      return
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    throw new Error("No order items");
+    return;
   } else {
-      const order = new Order({
-        orderItems,
-        user: req.user._id,
-        shippingAddress,
-        paymentMethod,
-        itemsPrice,
-        taxPrice,
-        shippingPrice,
-        totalPrice,
-      })
+    const order = new Order({
+      orderItems,
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
 
-      const createdOrder = await order.save()
+    const createdOrder = await order.save();
 
-      res.status(201).json(createdOrder)
+    res.status(201).json(createdOrder);
   }
 });
 
-export { addOrderItems }
+// @desc    Get order bt id
+// @route   GET /api/orders/:id
+// @acces   Private
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+
+  if (order) {
+    res.json(order);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
+});
+
+export { addOrderItems, getOrderById };
