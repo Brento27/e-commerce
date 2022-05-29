8096f086d60dccee02bc278afb7cbd99bf937994
Brento27 <lombaardbrent@gmail.com>
2022-05-29 19:14:44 +0200

    User details & ordersReset fixed on profile screen

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from "axios"
 import {
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCES,
@@ -13,32 +13,34 @@ import {
   USER_UPDATE_PROFILE_SUCCES,
   USER_UPDATE_PROFILE_FAIL,
   USER_UPDATE_PROFILE_REQUEST,
-} from "../constants/userConstants";
+  USER_DETAILS_RESET,
+} from "../constants/userConstants"
+import { ORDER_LIST_MY_RESET } from "../constants/orderConstants"
 
 export const login = (email, password) => async (dispatch) => {
   try {
     dispatch({
       type: USER_LOGIN_REQUEST,
-    });
+    })
 
     const config = {
       headers: {
         "Content-type": "application/json",
       },
-    };
+    }
 
     const { data } = await axios.post(
       "/api/users/login",
       { email, password },
       config
-    );
+    )
 
     dispatch({
       type: USER_LOGIN_SUCCES,
       payload: data,
-    });
+    })
 
-    localStorage.setItem("userInfo", JSON.stringify(data));
+    localStorage.setItem("userInfo", JSON.stringify(data))
   } catch (error) {
     dispatch({
       type: USER_LOGIN_FAIL,
@@ -46,44 +48,46 @@ export const login = (email, password) => async (dispatch) => {
         error.response && error.response.data.message
           ? error.response.data.message
           : error.message,
-    });
+    })
   }
-};
+}
 
 export const logout = () => (dispatch) => {
-  localStorage.removeItem('userInfo')
+  localStorage.removeItem("userInfo")
   dispatch({ type: USER_LOGOUT })
+  dispatch({ type: USER_DETAILS_RESET })
+  dispatch({ type: ORDER_LIST_MY_RESET })
 }
 
 export const register = (name, email, password) => async (dispatch) => {
   try {
     dispatch({
       type: USER_REGISTER_REQUEST,
-    });
+    })
 
     const config = {
       headers: {
         "Content-type": "application/json",
       },
-    };
+    }
 
     const { data } = await axios.post(
       "/api/users",
       { name, email, password },
       config
-    );
+    )
 
     dispatch({
       type: USER_REGISTER_SUCCES,
       payload: data,
-    });
+    })
 
     dispatch({
       type: USER_LOGIN_SUCCES,
       payload: data,
-    });
+    })
 
-    localStorage.setItem("userInfo", JSON.stringify(data));
+    localStorage.setItem("userInfo", JSON.stringify(data))
   } catch (error) {
     dispatch({
       type: USER_REGISTER_FAIL,
@@ -91,35 +95,33 @@ export const register = (name, email, password) => async (dispatch) => {
         error.response && error.response.data.message
           ? error.response.data.message
           : error.message,
-    });
+    })
   }
-};
+}
 
 export const getUserDetails = (id) => async (dispatch, getState) => {
   try {
     dispatch({
       type: USER_DETAILS_REQUEST,
-    });
+    })
 
-    const { userLogin: { userInfo } } = getState()
+    const {
+      userLogin: { userInfo },
+    } = getState()
 
     const config = {
       headers: {
         "Content-type": "application/json",
-        Authorization: `Bearer ${userInfo.token}`
+        Authorization: `Bearer ${userInfo.token}`,
       },
-    };
+    }
 
-    const { data } = await axios.get(
-      `/api/users/${id}`,
-      config
-    );
+    const { data } = await axios.get(`/api/users/${id}`, config)
 
     dispatch({
       type: USER_DETAILS_SUCCES,
       payload: data,
-    });
-
+    })
   } catch (error) {
     dispatch({
       type: USER_DETAILS_FAIL,
@@ -127,35 +129,33 @@ export const getUserDetails = (id) => async (dispatch, getState) => {
         error.response && error.response.data.message
           ? error.response.data.message
           : error.message,
-    });
+    })
   }
-};
+}
 
 export const updateUserProfile = (user) => async (dispatch, getState) => {
   try {
     dispatch({
       type: USER_UPDATE_PROFILE_REQUEST,
-    });
+    })
 
-    const { userLogin: { userInfo } } = getState()
+    const {
+      userLogin: { userInfo },
+    } = getState()
 
     const config = {
       headers: {
         "Content-type": "application/json",
-        Authorization: `Bearer ${userInfo.token}`
+        Authorization: `Bearer ${userInfo.token}`,
       },
-    };
+    }
 
-    const { data } = await axios.put(
-      `/api/users/profile`, user,
-      config
-    );
+    const { data } = await axios.put(`/api/users/profile`, user, config)
 
     dispatch({
       type: USER_UPDATE_PROFILE_SUCCES,
       payload: data,
-    });
-
+    })
   } catch (error) {
     dispatch({
       type: USER_UPDATE_PROFILE_FAIL,
@@ -163,6 +163,6 @@ export const updateUserProfile = (user) => async (dispatch, getState) => {
         error.response && error.response.data.message
           ? error.response.data.message
           : error.message,
-    });
+    })
   }
-};
+}
--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -7,6 +7,7 @@ import {
   ORDER_DETAILS_SUCCES,
   ORDER_LIST_MY_FAIL,
   ORDER_LIST_MY_REQUEST,
+  ORDER_LIST_MY_RESET,
   ORDER_LIST_MY_SUCCES,
   ORDER_PAY_FAIL,
   ORDER_PAY_REQUEST,
@@ -97,6 +98,8 @@ export const orderListMyReducer = (state = { orders: [] }, action) => {
         loading: false,
         error: action.payload,
       }
+    case ORDER_LIST_MY_RESET:
+      return { orders: [] }
     default:
       return state
   }
