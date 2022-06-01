39e621b4457adc82c39f721c0ef4df59c57c83fd
Brento27 <lombaardbrent@gmail.com>
2022-06-01 06:57:38 +0200

    Frontend Image upload and styling updated

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -26,7 +26,7 @@ const Header = () => {
           </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
+            <Nav className="ml-auto">
               <LinkContainer to="/cart">
                 <Nav.Link>
                   {cartItems.length > 0 && (
--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,6 +1,6 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const { createProxyMiddleware } = require("http-proxy-middleware")
 
-module.exports = function (app) {
+module.exports = function(app) {
   app.use(
     "/api",
 
@@ -9,5 +9,14 @@ module.exports = function (app) {
 
       changeOrigin: true,
     })
-  );
-};
+  )
+  app.use(
+    "/uploads",
+
+    createProxyMiddleware({
+      target: "http://localhost:5000",
+
+      changeOrigin: true,
+    })
+  )
+}
